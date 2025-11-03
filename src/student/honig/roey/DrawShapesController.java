2f511bad1f8ad0669c15b84a6b97980a7cb35d34
Roey Honig <roeyhonig94@gmail.com>
2025-11-03 22:23:32 +0200

    draw rec

--- src/student/honig/roey/DrawShapesController.java
+++ src/student/honig/roey/DrawShapesController.java
@@ -2,23 +2,26 @@ package student.honig.roey;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 
 public class DrawShapesController {
     @FXML
-    private Canvas drawingCanvas;
-
-    private void drawRec() {
+    private Canvas drawShapesCanvas;
+    @FXML
+    private Button drawShapesButton;
+    @FXML
+    private void drawShapesButtonPressed() {
         // Get the GraphicsContext for drawing
-        GraphicsContext gc = drawingCanvas.getGraphicsContext2D();
+        GraphicsContext gc = drawShapesCanvas.getGraphicsContext2D();
 
         // Clear the canvas
-        gc.clearRect(0, 0, drawingCanvas.getWidth(), drawingCanvas.getHeight());
+        gc.clearRect(0, 0, drawShapesCanvas.getWidth(), drawShapesCanvas.getHeight());
 
         // Draw a rectangle
         gc.setFill(Color.LIGHTBLUE);
         gc.fillRect(50, 50, 200, 100);
-
+/*
         // Draw a circle
         gc.setFill(Color.CORAL);
         gc.fillOval(300, 100, 100, 100);
@@ -26,6 +29,6 @@ public class DrawShapesController {
         // Draw a line
         gc.setStroke(Color.BLACK);
         gc.setLineWidth(3);
-        gc.strokeLine(50, 200, 400, 200);
+        gc.strokeLine(50, 200, 400, 200);*/
     }
 }
