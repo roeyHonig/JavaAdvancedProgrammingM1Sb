00d21182e99d0de96570374f1fd8271579ab5405
Roey Honig <roeyhonig94@gmail.com>
2025-11-03 23:18:16 +0200

    drawing 10 shapes

--- src/student/honig/roey/DrawShapesController.java
+++ src/student/honig/roey/DrawShapesController.java
@@ -5,6 +5,8 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 
+import java.util.Random;
+
 public class DrawShapesController {
     @FXML
     private Canvas drawShapesCanvas;
@@ -14,21 +16,70 @@ public class DrawShapesController {
     private void drawShapesButtonPressed() {
         // Get the GraphicsContext for drawing
         GraphicsContext gc = drawShapesCanvas.getGraphicsContext2D();
-
+        // Get the canvas width and height
+        double canvasWidth = drawShapesCanvas.getWidth();
+        double canvasHeight = drawShapesCanvas.getHeight();
         // Clear the canvas
         gc.clearRect(0, 0, drawShapesCanvas.getWidth(), drawShapesCanvas.getHeight());
 
-        // Draw a rectangle
-        gc.setFill(Color.LIGHTBLUE);
-        gc.fillRect(50, 50, 200, 100);
-/*
-        // Draw a circle
-        gc.setFill(Color.CORAL);
-        gc.fillOval(300, 100, 100, 100);
-
-        // Draw a line
-        gc.setStroke(Color.BLACK);
-        gc.setLineWidth(3);
-        gc.strokeLine(50, 200, 400, 200);*/
+        for (int i = 0; i < 10; i++) {
+            long choise = getRandomIntInRange(0,2);
+            if (choise == 0) {
+                drawARandomRectangle(gc, canvasWidth, canvasHeight);
+            } else if (choise == 1) {
+                drawARandomElipse(gc, canvasWidth, canvasHeight);
+            } else {
+                drawARandomLine(gc, canvasWidth, canvasHeight);
+            }
+        }
     }
+
+    private void drawARandomRectangle(GraphicsContext gc, double canvasWidth, double canvasHeight) {
+        double rectWidth = getRandomNumberInRange(1, canvasWidth / 4);
+        double rectHeight = getRandomNumberInRange(1, canvasHeight / 4);
+        double x = getRandomNumberInRange(0, canvasWidth);
+        double y = getRandomNumberInRange(0, canvasHeight);
+        Color randomColor = getRandomColor();
+        gc.setFill(randomColor);
+        gc.fillRect(x, y, rectWidth, rectHeight);
+    }
+
+    private void drawARandomElipse(GraphicsContext gc, double canvasWidth, double canvasHeight) {
+        double elipWidth = getRandomNumberInRange(1, canvasWidth / 4);
+        double elipHeight = getRandomNumberInRange(1, canvasHeight / 4);
+        double x = getRandomNumberInRange(0, canvasWidth);
+        double y = getRandomNumberInRange(0, canvasHeight);
+        Color randomColor = getRandomColor();
+        gc.setFill(randomColor);
+        gc.fillOval(x, y, elipWidth, elipHeight);
+    }
+
+    private void drawARandomLine(GraphicsContext gc, double canvasWidth, double canvasHeight) {
+        double lineBoxWidth = getRandomNumberInRange(1, canvasWidth / 4);
+        double lineBoxHeight = getRandomNumberInRange(1, canvasHeight / 4);
+        double x = getRandomNumberInRange(0, canvasWidth);
+        double y = getRandomNumberInRange(0, canvasHeight);
+        Color randomColor = getRandomColor();
+        gc.setStroke(randomColor);
+        gc.strokeLine(x, y, (x+lineBoxWidth), (y+lineBoxHeight));
+    }
+
+    private Color getRandomColor() {
+        Random rand = new Random();
+        double r = rand.nextDouble();  // Random value between 0.0 and 1.0
+        double g = rand.nextDouble();  // Random value between 0.0 and 1.0
+        double b = rand.nextDouble();  // Random value between 0.0 and 1.0
+        return new Color(r, g, b, 1.0);  // Create color using random RGB values
+    }
+
+    private double getRandomNumberInRange(double min, double max) {
+        Random rand = new Random();
+        return min + (max - min) * rand.nextDouble();
+    }
+
+    private int getRandomIntInRange(int min, int max) {
+        Random rand = new Random();
+        return rand.nextInt((max - min) + 1) + min;  // Generate int in the range [min, max]
+    }
+
 }
